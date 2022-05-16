3f7874509933cb1ce2ee733977ade45d4710b1a1
ekkaphoom <ekkaphoom@gmail.com>
2022-05-16 16:45:36 +0700

    Update example

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCnaYWtYjrmwJ6h-Z8sY_7TNVX9EVGFFsQ",
-  authDomain: "facebook-to-7bf04.firebaseapp.com",
-  projectId: "facebook-to-7bf04",
-  storageBucket: "facebook-to-7bf04.appspot.com",
-  messagingSenderId: "889944000778",
-  appId: "1:889944000778:web:669cb1978953826d5f12f3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: 'netflix-clone-edd1e.firebaseapp.com',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: 'netflix-clone-edd1e.appspot.com',
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
